f93d629f9f7050e8b7893988c42d35095e601fd9
linbaishuang <1765259718@qq.com>
2017-11-26 16:08:56 +0800

    完成

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,11 +1,11 @@
 const main = require('../main/main');
-
+const datbase = require('../main/datbase');
 describe('pos', function () {
     var allItems;
     var inputs;
 
     beforeEach(function () {
-        allItems = loadAllItems();
+        allItems = datbase.loadAll();
         inputs = [
             'ITEM000001',
             'ITEM000001',
@@ -23,7 +23,7 @@ describe('pos', function () {
 
         spyOn(console, 'log');
 
-        printInventory(inputs);
+        main.printInventory(inputs);
 
         var expectText =
             '***<没钱赚商店>购物清单***\n' +
